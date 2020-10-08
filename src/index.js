590a340aedaf1771b48b92cb0c9d15d6068733f4
JRIGS <raczkowskijonathan@gmail.com>
2020-10-07 20:33:01 -0400

    Finished up to MVP

--- src/index.js
+++ src/index.js
@@ -2,8 +2,20 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
+//importing redux provider
+import { Provider } from 'react-redux';
+
 import 'bulma/css/bulma.css';
 import './styles.scss';
 
+//importing our createStore & reducer
+import { createStore } from 'redux';
+import { reducer } from './reducer/reducer.index';
+//creating store which calls the reducer
+const store = createStore(reducer);
+
 const rootElement = document.getElementById('root');
-ReactDOM.render(<App />, rootElement);
+//applying our Provider to the APP
+ReactDOM.render(<Provider store={store}>
+    <App />
+    </Provider>, rootElement);
